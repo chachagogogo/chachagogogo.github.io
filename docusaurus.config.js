84c1dceaeb6a6509cad18ec90eb45867282ddadf
Yang Haeseung <chachagogogo@gmail.com>
2022-06-01 13:51:05 +0900

    testing...

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,6 +19,7 @@ const config = {
   organizationName: 'chachagogogo', // Usually your GitHub org/user name.
   projectName: 'chachagogogo.github.io', // Usually your repo name.
   trailingSlash: false,
+  deploymentBranch: 'gh-pages',
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
