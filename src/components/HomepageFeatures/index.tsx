99b078e293b180b3d1b5c028da03f9d73b913868
Yang Haeseung <chachagogogo@gmail.com>
2022-06-01 22:21:10 +0900

    홈 - 문구, 이미지 변경

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -10,32 +10,29 @@ type FeatureItem = {
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: '노트',
+    Svg: require('@site/static/img/folder.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        일하거나 공부하면서 알게 된 지식을 정리합니다. 프론트엔드 웹개발자인만큼 <code>HTML</code>, <code>CSS</code>, <code>JS</code>, <code>React</code> 등이 주요 기록 대상입니다.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    title: '블로그',
+    Svg: require('@site/static/img/cup.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        개발과 관련된 제 생각, 혹은 일지를 적습니다. 차곡차곡 정돈된 노트와는 다르게 그때마다 떠오르는 생각이나 소식 등으로 채울 예정입니다.
       </>
     ),
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: '소개',
+    Svg: require('@site/static/img/id-card.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        저에 관한 간략한 소개를 하는 페이지입니다. 꾸준한 성장과 긍정적인 관계를 지향합니다.
       </>
     ),
   },
