7202388691a0323ce6a902d6d13b0af058656d33
Yang Haeseung <chachagogogo@gmail.com>
2022-06-18 12:04:59 +0900

    editUrl 수정

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -39,7 +39,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/chachagogogo/chachagogogo.github.io/tree/gh-pages/',
+            'https://github.com/chachagogogo/chachagogogo.github.io/tree/main/',
           showLastUpdateTime: true,
         },
         blog: {
@@ -47,7 +47,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/chachagogogo/chachagogogo.github.io/tree/gh-pages/',
+            'https://github.com/chachagogogo/chachagogogo.github.io/tree/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
